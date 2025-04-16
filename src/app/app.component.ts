2f8423ff716b87a173b4675255e9104e0209f8eb
Ushan Rashmika <ushanrashmika23@gmail.com>
2025-04-16 16:52:12 +0530

    project init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DashboardComponent } from './components/screens/dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
